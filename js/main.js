1a01e37a1f391a90b260ac40e3eb00646558f228
Calpurniax <sofia.gracia@gmail.com>
2022-11-07 10:41:58 +0100

    add fetch

--- js/main.js
+++ js/main.js
@@ -1,38 +1,50 @@
 'use strict';
 
 
-//crear la lista de gatitos en JS
-
+//variables de pintar gatitos
 const listKitten = document.querySelector('.js-list');
-//constante de cada gatito
-
-const kittenData_1 = {
-  url: "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg",
-  name: 'Anastacio'.toUpperCase(),
-  desc: 'Risueño, juguetón, le guta estar tranquilo y que nadie le moleste.  Es una maravilla acariciarle!',
-  race: ''
-};
-
-const kittenData_2 = {
-  url: "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg",
-  name: 'Fiona'.toUpperCase(),
-  desc: 'Risueño, cariñoso, le guta estar tranquilo y que nadie le moleste.  Es una maravilla acariciarle!',
-  race: 'British Shorthair'
-}
-
-const kittenData_3 = {
-  url: "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg",
-  name: 'Cielo'.toUpperCase(),
-  desc: 'Risueño, cariñoso, le guta estar tranquilo y que nadie le moleste.  Es una maravilla acariciarle!',
-  race: ""
-}
+const GITHUB_USER = 'marialapc';
+const SERVER_URL = `https://dev.adalab.es/api/kittens/${GITHUB_USER}`;
+let kittenDataList = [];
 
 
-const kittenDataList = [kittenData_1, kittenData_2, kittenData_3]
+//variable de boton buscar
+const searchBtn = document.querySelector('.js-btn-search');
+//variable de boton añadir
+const btnAdd = document.querySelector(".js-btn-add");
+//variable de boton cancelar
+const btonCancel = document.querySelector(".js-btn-cancel");
+
+//Constantes para mostrar/ocultar el formulario
+const formAddCat = document.querySelector('.js-new-form');
+const btn = document.querySelector(".js-cross");
+
+//mensaje de error
+const labelMesageError = document.querySelector('.js-label-error');
+
+//const de los input
+const inputDesc = document.querySelector('.js-input-desc');
+const inputPhoto = document.querySelector('.js-input-photo');
+const inputName = document.querySelector('.js-input-name');
+const inputRace = document.querySelector('.js-input-race');
+let valueDesc = inputDesc.value;
+let valuePhoto = inputPhoto.value;
+let valueName = inputName.value;
+let valueRace = inputRace.value;
+
+//petición al servidor
+fetch(SERVER_URL, {
+  method: 'GET',
+  headers: { 'Content-Type': 'application/json' },
+}).then(response => response.json())
+  .then((data) => {
+    kittenDataList = data.results;
+    console.log(kittenDataList)
+    renderKittenList(kittenDataList);
+  });
 
 
 //validar la raza
-
 function renderRace(race) {
   if (race === "") {
     race = `<p class="card_race">No se ha especificado la raza</p>`
@@ -43,36 +55,22 @@ function renderRace(race) {
   return race;
 }
 
-
+//para pintar cada gato
 function renderKitten(kittenData) {
   kittenData.race = renderRace(kittenData.race);
-  const htmlKitten = `<li class="card"><img class="card_img"src=${kittenData.url} alt="gatito"/><h3 class="card_title">${kittenData.name}</h3>${kittenData.race}<p class="card_description"> ${kittenData.desc}</p></li>`;
+  const htmlKitten = `<li class="card"><img class="card_img"src=${kittenData.image} alt="gatito"/><h3 class="card_title">${kittenData.name}</h3>${kittenData.race}<p class="card_description"> ${kittenData.desc}</p></li>`;
   return htmlKitten
 }
 
-/*const kittenOne = renderKitten(kittenData_1);
-const kittenTwo = renderKitten(kittenData_2);
-const kittenThree = renderKitten(kittenData_3);
-renderKittenList(kittenDataList);
-
-  
-// cambiamos el contenido de js-list (llamando a la constante)
-
-listKitten.innerHTML = kittenOne + kittenTwo + kittenThree;*/
-
+//funcion para pintar la lista de gatos
 function renderKittenList(kittenDataList) {
   for (const kitten of kittenDataList) {
     const htmlKitten = renderKitten(kitten);
     listKitten.innerHTML += htmlKitten;
   }
 }
-renderKittenList(kittenDataList);
-
 
 //búsqueda por descripción
-
-const searchBtn = document.querySelector('.js-btn-search');
-
 function filterKitten(event) {
   event.preventDefault();
   const input_search_desc = document.querySelector('.js_in_search_desc');
@@ -95,31 +93,9 @@ function filterKitten(event) {
   };
   console.log(descrSearchText);
 };
-
 searchBtn.addEventListener('click', filterKitten);
 
-//Constantes para mostrar/ocultar el formulario
-
-const formAddCat = document.querySelector('.js-new-form');
-const btn = document.querySelector(".js-cross");
-
-//validar la información del formulario
-const btnAdd = document.querySelector(".js-btn-add");
-//const de los input
-const inputDesc = document.querySelector('.js-input-desc');
-const inputPhoto = document.querySelector('.js-input-photo');
-const inputName = document.querySelector('.js-input-name');
-const inputRace = document.querySelector('.js-input-race');
-let valueDesc = inputDesc.value;
-let valuePhoto = inputPhoto.value;
-let valueName = inputName.value;
-let valueRace = inputRace.value;
-
-//mensaje de error
-const labelMesageError = document.querySelector('.js-label-error');
-
 //evento de click en añadir
-
 function addNewKitten(event) {
   event.preventDefault();
   if (inputDesc.value === "" || inputPhoto.value === "" || inputName.value === "") {
@@ -153,9 +129,6 @@ btnAdd.addEventListener('click', (addNewKitten));
 
 //Cancelar formulario
 
-const btonCancel = document.querySelector(".js-btn-cancel");
-
-//Escuchar el boton 
 const cancelNewKitten = (event) => {
   event.preventDefault();
   cleanInput();
