8d1904618698560f27b8626c95c3bc7a0fd919b9
marialapc <marialopez.apc@gmail.com>
2022-11-08 11:09:41 +0100

    peticiones al servidor II

--- js/main.js
+++ js/main.js
@@ -1,58 +1,74 @@
-'use strict';
-
+"use strict";
 
 //variables de pintar gatitos
-const listKitten = document.querySelector('.js-list');
-const GITHUB_USER = 'marialapc';
+const listKitten = document.querySelector(".js-list");
+const GITHUB_USER = "marialapc";
 const SERVER_URL = `https://dev.adalab.es/api/kittens/${GITHUB_USER}`;
 let kittenDataList = [];
 
-
 //variable de boton buscar
-const searchBtn = document.querySelector('.js-btn-search');
+const searchBtn = document.querySelector(".js-btn-search");
+const input_search_desc = document.querySelector('.js_in_search_desc');
+const input_search_race = document.querySelector('.js_in_search_race');
+let valueSearchDesc = '';
+let valueSearchRace = '';
+const labelSearch = document.querySelector(".error_search");
+
 //variable de boton añadir
 const btnAdd = document.querySelector(".js-btn-add");
 //variable de boton cancelar
 const btonCancel = document.querySelector(".js-btn-cancel");
 
 //Constantes para mostrar/ocultar el formulario
-const formAddCat = document.querySelector('.js-new-form');
+const formAddCat = document.querySelector(".js-new-form");
 const btn = document.querySelector(".js-cross");
 
 //mensaje de error
-const labelMesageError = document.querySelector('.js-label-error');
+const labelMesageError = document.querySelector(".js-label-error");
 
 //const de los input
-const inputDesc = document.querySelector('.js-input-desc');
-const inputPhoto = document.querySelector('.js-input-photo');
-const inputName = document.querySelector('.js-input-name');
-const inputRace = document.querySelector('.js-input-race');
+const inputDesc = document.querySelector(".js-input-desc");
+const inputPhoto = document.querySelector(".js-input-photo");
+const inputName = document.querySelector(".js-input-name");
+const inputRace = document.querySelector(".js-input-race");
 let valueDesc = inputDesc.value;
 let valuePhoto = inputPhoto.value;
 let valueName = inputName.value;
 let valueRace = inputRace.value;
 
-//petición al servidor
-fetch(SERVER_URL, {
-  method: 'GET',
-  headers: { 'Content-Type': 'application/json' },
-}).then(response => response.json())
-  .then((data) => {
-    kittenDataList = data.results;
-    console.log(kittenDataList)
-    renderKittenList(kittenDataList);
-  });
+// variable para almacenar los gatitos que se encuentren el local storage
+const kittenListStored = JSON.parse(localStorage.getItem("kittensList"));
 
+if (kittenListStored !== null) {
+  renderKittenList(kittenListStored);
+  kittenDataList = kittenListStored;
+} else {
+  fetch(SERVER_URL, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      kittenDataList = data.results;
+      console.log(kittenDataList);
+      renderKittenList(kittenDataList);
+      localStorage.setItem("kittensList", JSON.stringify(kittenDataList));
+    })
+
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
 //validar la raza
 ///echarle un ojo, no repetir el h4, incluir solo texto
 function renderRace(race) {
   if (race === "") {
-    race = `<p class="card_race">No se ha especificado la raza</p>`
+    race = `<p class="card_race">No se ha especificado la raza</p>`;
   } else {
-    race = `<h4 class="card_race">${race}</h4>`
+    race = `<h4 class="card_race">${race}</h4>`;
   }
-  console.log(typeof (race));
+  console.log(typeof race);
   return race;
 }
 
@@ -60,7 +76,7 @@ function renderRace(race) {
 function renderKitten(kittenData) {
   kittenData.race = renderRace(kittenData.race);
   const htmlKitten = `<li class="card"><img class="card_img"src=${kittenData.image} alt="gatito"/><h3 class="card_title">${kittenData.name}</h3>${kittenData.race}<p class="card_description"> ${kittenData.desc}</p></li>`;
-  return htmlKitten
+  return htmlKitten;
 }
 
 //funcion para pintar la lista de gatos
@@ -74,46 +90,65 @@ function renderKittenList(kittenDataList) {
 //búsqueda por descripción
 function filterKitten(event) {
   event.preventDefault();
-  valueSearchDesc = input_search_desc.value;
+  valueSearchDesc = input_search_desc.value;//mirar esto
   valueSearchRace = input_search_race.value;
-
+  console.log(kittenDataList);
   if (valueSearchDesc === "" || valueSearchRace === "") {
-    labelSearch.innerHTML = 'Debe rellenar todos los valores.'
+    labelSearch.innerHTML = "Debe rellenar todos los valores.";
   }
   const searchedKitten = kittenDataList.filter(filterFunction);
-  listKitten.innerHTML = '';
+  listKitten.innerHTML = "";
   renderKittenList(searchedKitten);
   console.log(searchedKitten);
-};
+}
 
 function filterFunction(kitten) {
-  if (kitten.desc.includes(valueSearchDesc) && kitten.race.includes(valueSearchRace)) {
-    return kitten
+  if (
+    kitten.desc.includes(valueSearchDesc) &&
+    kitten.race.includes(valueSearchRace)
+  ) {
+    return kitten;
   }
-};
+}
 
-searchBtn.addEventListener('click', filterKitten);
+searchBtn.addEventListener("click", filterKitten);
 
 //evento de click en añadir
 function addNewKitten(event) {
   event.preventDefault();
-  if (inputDesc.value === "" || inputPhoto.value === "" || inputName.value === "") {
-    labelMesageError.innerHTML = 'Debe rellenar todos los valores.'
+  if (
+    inputDesc.value === "" ||
+    inputPhoto.value === "" ||
+    inputName.value === ""
+  ) {
+    labelMesageError.innerHTML = "Debe rellenar todos los valores.";
   } else {
     const newKittenDataObject = {
       name: inputName.value,
       desc: inputDesc.value,
-      url: inputPhoto.value,
-      race: inputRace.value
-    }
-    kittenDataList.push(newKittenDataObject);
-    cleanInput();
-    console.log(newKittenDataObject);
-    labelMesageError.innerHTML = 'Mola! Un nuevo gatito en Adalab!';
-    listKitten.innerHTML = '';
-    renderKittenList(kittenDataList);
-  };
-
+      image: inputPhoto.value,
+      race: inputRace.value,
+    };
+    fetch(`https://dev.adalab.es/api/kittens/${GITHUB_USER}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newKittenDataObject),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success !== null) {
+          kittenDataList.push(newKittenDataObject);
+          cleanInput();
+          console.log(newKittenDataObject);
+          labelMesageError.innerHTML = "Mola! Un nuevo gatito en Adalab!";
+          listKitten.innerHTML = "";
+          renderKittenList(kittenDataList);
+          localStorage.setItem("kittensList", JSON.stringify(kittenDataList));
+        } else {
+          console.log("ha habido un error");
+        }
+      });
+  }
 }
 
 function cleanInput() {
@@ -123,8 +158,7 @@ function cleanInput() {
   inputRace.value = "";
 }
 
-
-btnAdd.addEventListener('click', (addNewKitten));
+btnAdd.addEventListener("click", addNewKitten);
 
 //Cancelar formulario
 
@@ -134,15 +168,15 @@ const cancelNewKitten = (event) => {
   formAddCat.classList.add("collapsed");
 };
 
-btonCancel.addEventListener('click', (cancelNewKitten));
+btonCancel.addEventListener("click", cancelNewKitten);
 
 //Funciones para mostrar/oculta el menú de añadir gatito
 function showNewCatForm() {
-  formAddCat.classList.remove('collapsed');
+  formAddCat.classList.remove("collapsed");
 }
 
 function hideNewCatForm() {
-  formAddCat.classList.add('collapsed');
+  formAddCat.classList.add("collapsed");
 }
 //función que irá con el evento de click y que tiene el "if"
 function handleClickNewCatForm(event) {
@@ -152,8 +186,7 @@ function handleClickNewCatForm(event) {
   } else {
     hideNewCatForm();
   }
-};
+}
 
 //evento de escucha con eventhandler
-btn.addEventListener('click', handleClickNewCatForm);
-
+btn.addEventListener("click", handleClickNewCatForm);
